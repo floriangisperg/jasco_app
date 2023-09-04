1e12cf581a634bf40be462036667dce0da932612
floriangisperg <104788811+floriangisperg@users.noreply.github.com>
2023-09-04 16:20:53 +0200

    Update Main_Page.py

--- Main_Page.py
+++ Main_Page.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.set_page_config(layout="wide")
 st.markdown("""
-#Jasco Fluorimeter Data Analysis App
+# Jasco Fluorimeter Data Analysis App
 This application is designed to analyze the .csv output files from Jasco Spectrofluorometer. It is structured into two modules:
 
 1. **Time Series Measurement Analysis/Refolding Analysis**
