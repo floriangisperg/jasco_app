c7c263c2e8f6a0cc99e602e5d67ed14b563d6d3e
floriangisperg <104788811+floriangisperg@users.noreply.github.com>
2023-09-04 14:24:56 +0200

    added intro text Jasco_Fluorimeter_App.py

--- Jasco_Fluorimeter_App.py
+++ Jasco_Fluorimeter_App.py
@@ -3,19 +3,36 @@ st.header("Intro page to be edited")
 st.markdown("""
 # Jasco Fluorimeter Data Analysis
 
-This application is designed to analyze data from Jasco Fluorimeter, a device which [put a brief description of the device here].
+This application is designed to analyze the .csv output files from Jasco Spectrofluorometer. It is structured into two modules:
+
+1. **Time Series Measurement Analysis/Refolding Analysis**
+2. **Single Spectrum Analysis**
 
 ## Measurement Principles
 
-[Explain the measurement principles in detail here]
+### Fluorescence emission spectra of intrinsic tryptophan and tyrosine as a monitoring tool for refolding of inclusion bodies
+
+The fluorescence spectra originate from tryptophan and tyrosine residues in the sequence of the protein of interest. The monitoring is based on the sensitivity of tryptophan and tyrosine residues to hydrophobicity/polarity of their local environment.
+
+- In the native (folded) protein, the hydrophobic tryptophan and tyrosine side chains are buried in the protein core.
+- In the denatured (unfolded) protein, these residues are more exposed to the solvent, i.e., in a less hydrophobic local environment.
+
+Therefore, the denaturation of the protein is accompanied by the red-shift (toward higher wavelength) of a tryptophan and tyrosine fluorescence peak maxima as those residues become more solvent exposed. The refolding of the protein of interest, which is essentially the inverse process (from denatured to native state), is then characterized by the shift of the fluorescence maximum in the opposite direction.
+
+The shift in the maxima of fluorescence peaks is most efficiently characterized by calculating the centre of mass of the peak, here called **Average Emission Wavelength (AEW)**, of each spectrum in the time series.
+
+\[
+AEW = \frac{\sum_{i}^{j} \text{Int}_i \times \text{Wavel}_i}{\sum_{i}^{j} \text{Int}_i}
+\]
 
 ## Use Cases
 
 ### Time Series Measurement
 
-This application can be used to analyze time series measurements where data is collected in a distinct time interval over multiple hours for refolding experiments. [Add more details about this use case here]
+This module is used to analyze time series measurements where data are collected in distinct time intervals over multiple minutes/hours.
 
-### Singular Experiments
+### Individual Spectra
 
-The application is also designed to handle singular experiments. [Add more details about this use case here]
+This module is designed to visualize and process the individual spectra from a .csv output files.
+
 """)
