52f41ba406dfdbfc466846d58cec6fd786082d39
Florian Gisperg <104788811+floriangisperg@users.noreply.github.com>
2023-10-03 15:35:27 +0200

    Update Main_Page.py

--- Main_Page.py
+++ Main_Page.py
@@ -7,8 +7,7 @@ st.markdown('This application is designed to analyze the .csv output files from 
 
 st.header('Measurement Principles')
 st.caption('Fluorescence emission spectra of intrinsic tryptophan and tyrosine as a monitoring tool for refolding of inclusion bodies')
-
-st.markdown('---')  # Divider line
+#st.markdown('---')  # Divider line
 
 st.markdown('The fluorescence spectra originate from tryptophan and tyrosine residues in the sequence of the protein of interest. The monitoring is based on the sensitivity of tryptophan and tyrosine residues to hydrophobicity/polarity of their local environment. In the native (folded) protein the hydrophobic tryptophan and tyrosine side chains are buried in the protein core whereas in the denatured (unfolded) protein these residues are more exposed to the solvent, i.e. in less hydrophobic local environment. Therefore, the denaturation of the protein is accompanied with the red-shift (toward higher wavelength) of a tryptophan and tyrosine fluorescence peak maxima as those residues become more solvent exposed. The refolding of the protein of interest, which is essentially the inverse process (from denatured to native state), is then characterized by the shift of the fluorescence maximum in the opposite direction.')
 st.markdown('The shift in the maxima of a fluorescence peaks is most efficiently characterized by calculating the centre of a mass of the peak, here called average emission wavelength (AEW), of each spectrum in the time series.')
